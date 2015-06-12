d638df95c7b38a8b13860a1b279b58d133f1f6fa
Guy Lewin <guy@lewin.co.il>
2015-06-12 22:42:51 +0300

    Split into functions

--- server/logic.py
+++ server/logic.py
@@ -21,6 +21,34 @@ def get_events(user_id):
 
     return events
 
+def fill_poll_options_in_poll(poll, user_id):
+    selected_poll_option = db_handler.query_db("""
+    SELECT poll_option.poll_option_id FROM poll_option
+    INNER JOIN user_poll_option
+    INNER JOIN user
+    WHERE poll_option.poll_id = ?
+    AND poll_option.poll_option_id = user_poll_option.poll_option_id
+    AND user_poll_option.user_id = user.user_id
+    AND user.phone = ?
+    """, (poll['poll_id'], user_id))
+    if len(selected_poll_option) == 0:
+        selected_poll_option = -1
+    else:
+        selected_poll_option = selected_poll_option[0]["poll_option_id"]
+
+    poll_options = db_handler.query_db("""
+    SELECT poll_option.poll_option_id, poll_option.poll_option_name,
+    COUNT(poll_option.poll_option_id) AS poll_option_count
+    FROM poll_option
+    INNER JOIN user_poll_option
+    WHERE poll_option.poll_id = ?
+    AND user_poll_option.poll_option_id = poll_option.poll_option_id
+    GROUP BY
+    poll_option.poll_option_id
+    """, (poll['poll_id'],))
+    poll["options"] = poll_options
+    poll["selected_poll_option"] = selected_poll_option
+
 def get_event_details(event_id, user_id):
     event_details = db_handler.query_db("""
     SELECT event.event_id, event.event_name, event_user.status
@@ -40,37 +68,13 @@ def get_event_details(event_id, user_id):
     """, (event_id,))
 
     for poll in polls:
-        selected_poll_option = db_handler.query_db("""
-        SELECT poll_option.poll_option_id FROM poll_option
-        INNER JOIN user_poll_option
-        INNER JOIN user
-        WHERE poll_option.poll_id = ?
-        AND poll_option.poll_option_id = user_poll_option.poll_option_id
-        AND user_poll_option.user_id = user.user_id
-        AND user.phone = ?
-        """, (poll['poll_id'], user_id))
-        if len(selected_poll_option) == 0:
-            selected_poll_option = -1
-        else:
-            selected_poll_option = selected_poll_option[0]["poll_option_id"]
+        fill_poll_options_in_poll(poll, user_id)
 
-        poll_options = db_handler.query_db("""
-        SELECT poll_option.poll_option_id, poll_option.poll_option_name,
-        COUNT(poll_option.poll_option_id) AS poll_option_count
-        FROM poll_option
-        INNER JOIN user_poll_option
-        WHERE poll_option.poll_id = ?
-        AND user_poll_option.poll_option_id = poll_option.poll_option_id
-        GROUP BY
-        poll_option.poll_option_id
-        """, (poll['poll_id'],))
-        poll["options"] = poll_options
-        poll["selected_poll_option"] = selected_poll_option
     event_details["polls"] = polls
     return event_details
 
 def answer_polls(user_id, poll_option_id):
-    count = db_handler.query_db("""
+    result = db_handler.query_db("""
     SELECT COUNT(*) as count
     FROM poll_option
     INNER JOIN user_poll_option
@@ -83,18 +87,27 @@ def answer_polls(user_id, poll_option_id):
     AND user_poll_option.poll_option_id = poll_option.poll_option_id
     AND user_poll_option.user_id = user.user_id
     AND user.phone = ?
-    """, (user_id, user_id), True)["count"]
+    """, (poll_option_id, user_id), True)
 
     # TODO: Support changing your mind
-    if count > 0:
+    if result["count"] > 0:
         raise APIException("Do not vote twice for the same poll")
 
-    user_id = db_handler.query_db("""
+    db_user_id = db_handler.query_db("""
     SELECT user_id FROM user WHERE phone = ?
     """, (user_id,), True)
-    if user_id is None:
+    if db_user_id is None:
         raise APIException("User does not exist")
 
-    db_handler.insert_db("user_poll_option", {"user_id": user_id["user_id"], "poll_option_id": poll_option_id})
+    db_handler.insert_db("user_poll_option", {"user_id": db_user_id["user_id"], "poll_option_id": poll_option_id})
 
+    poll = db_handler.query_db("""
+    SELECT poll.poll_id, poll.poll_name, poll.overridden_poll_option
+    FROM poll
+    INNER JOIN poll_option
+    WHERE poll.poll_id = poll_option.poll_id
+    AND poll_option.poll_option_id = ?
+    """, (poll_option_id,), True)
+    fill_poll_options_in_poll(poll, user_id)
 
+    return poll
