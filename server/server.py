914b1f48d6f3b4a182dba77261f9e3fb3d122522
Guy Lewin <guy@lewin.co.il>
2015-06-12 16:10:56 +0300

    get_events finalised

--- server/server.py
+++ server/server.py
@@ -6,9 +6,24 @@ import flask
 
 app = flask.Flask(__name__)
 
+def format_error(error_message):
+    return flask.jsonify({"error": error_message})
+
 @app.route('/get_events')
 def get_events():
-    events = db_handler.query_db("SELECT * FROM event")
+    user_id = flask.request.args.get("user_id", None)
+    if user_id is None:
+        return format_error("user_id not supplied")
+
+    events = db_handler.query_db("""
+    SELECT event.event_id, event.event_name, event_user.status
+    FROM event
+    INNER JOIN event_user
+    INNER JOIN user
+    WHERE event.event_id=event_user.event_id
+    AND event_user.user_id = user.user_id
+    AND user.phone = ?
+    """, (user_id,))
     return flask.jsonify({"events": events})
 
 @app.before_request
