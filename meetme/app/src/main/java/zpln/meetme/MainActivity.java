e3417a08e3e003e98b87223aecc99f609df3a18f
Tomer Greenwald <tomerg@gmail.com>
2015-06-13 01:01:33 +0300

    Both views are available

--- meetme/app/src/main/java/zpln/meetme/MainActivity.java
+++ meetme/app/src/main/java/zpln/meetme/MainActivity.java
@@ -1,6 +1,8 @@
 package zpln.meetme;
 
+import android.app.ActionBar;
 import android.content.Context;
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,13 +16,28 @@ import android.widget.BaseAdapter;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextClock;
 import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ValueFormatter;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -52,7 +69,15 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.event_list);
+
+//        setContentView(R.layout.event_list);
+//        createPartyListView();
+
+        setContentView(R.layout.party_event_view);
+        createPartyEventView();
+    }
+
+    private void createPartyListView() {
         ListView listview = (ListView) findViewById(R.id.listView);
 
         Event event1 = new Event("Party", "Hapak", new Date(2015, 1, 2), new String[]{"Stav", "Tamar"});
@@ -112,6 +137,159 @@ public class MainActivity extends ActionBarActivity {
 
         );
     }
+
+    private void createPartyEventView() {
+        ListView partyPollListView = (ListView) findViewById(R.id.partyPollListView);
+
+
+        Map<String, Integer> keyCount = new HashMap<>();
+        keyCount.put("Hapak", 2);
+        keyCount.put("Panasi", 3);
+        keyCount.put("Moses", 1);
+        keyCount.put("Shmafia", 5);
+        Poll whereDoWeEat = new Poll(keyCount, "Where do we eat?");
+
+        Map<String, Integer> keyCount2 = new HashMap<>();
+        keyCount2.put("Bat-El", 6);
+        keyCount2.put("Nokdim", 4);
+        keyCount2.put("Bney Kedem", 3);
+        keyCount2.put("Efrat", 2);
+        Poll whereDoWeGuard = new Poll(keyCount2, "Where do we guard?");
+
+        final Poll polls[] = {whereDoWeEat, whereDoWeGuard, whereDoWeEat};
+        ViewGroup.LayoutParams lp = (ViewGroup.LayoutParams) partyPollListView.getLayoutParams();
+        lp.height = lp.height*polls.length;
+        partyPollListView.setLayoutParams(lp);
+        final MainActivity that = this;
+        partyPollListView.setAdapter(new BaseAdapter() {
+                                         private Typeface mTf;
+
+                                         @Override
+                                         public int getCount() {
+                                             return polls.length;
+                                         }
+
+                                         @Override
+                                         public Object getItem(int position) {
+                                             return null;
+                                         }
+
+                                         @Override
+                                         public long getItemId(int position) {
+                                             return 0;
+                                         }
+
+                                         @Override
+                                         public View getView(int position, View convertView, ViewGroup parent) {
+                                             LinearLayout relativeLayout = (LinearLayout) LayoutInflater.from(that).inflate(R.layout.activity_barchart, null);
+
+                                             BarChart mChart = (BarChart) relativeLayout.getChildAt(0);
+                                             updatedChartByPoll(polls[position], mChart);
+                                             return relativeLayout;
+                                         }
+
+                                         private void updatedChartByPoll(Poll poll, BarChart mChart) {
+
+                                             mChart.setDrawBarShadow(false);
+                                             mChart.setDrawValueAboveBar(true);
+
+                                             mChart.setDescription("");
+
+                                             // if more than 60 entries are displayed in the chart, no values will be
+                                             // drawn
+                                             mChart.setMaxVisibleValueCount(60);
+
+                                             // scaling can now only be done on x- and y-axis separately
+                                             mChart.setPinchZoom(false);
+
+                                             // draw shadows for each bar that show the maximum value
+                                             // mChart.setDrawBarShadow(true);
+
+                                             // mChart.setDrawXLabels(false);
+
+                                             mChart.setDrawGridBackground(false);
+                                             // mChart.setDrawYLabels(false);
+
+                                             mTf = Typeface.createFromAsset(getAssets(), "OpenSans-Regular.ttf");
+
+                                             XAxis xAxis = mChart.getXAxis();
+                                             xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+                                             xAxis.setTypeface(mTf);
+                                             xAxis.setDrawGridLines(false);
+                                             xAxis.setSpaceBetweenLabels(2);
+
+                                             ValueFormatter custom = new MyValueFormatter();
+
+                                             YAxis leftAxis = mChart.getAxisLeft();
+                                             leftAxis.setTypeface(mTf);
+                                             leftAxis.setLabelCount(4);
+                                             leftAxis.setValueFormatter(custom);
+                                             leftAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+                                             leftAxis.setSpaceTop(15f);
+
+                                             YAxis rightAxis = mChart.getAxisRight();
+                                             rightAxis.setDrawGridLines(false);
+                                             rightAxis.setTypeface(mTf);
+                                             rightAxis.setLabelCount(4);
+                                             rightAxis.setValueFormatter(custom);
+                                             rightAxis.setSpaceTop(15f);
+
+                                             Legend l = mChart.getLegend();
+                                             l.setPosition(Legend.LegendPosition.BELOW_CHART_LEFT);
+                                             l.setForm(Legend.LegendForm.SQUARE);
+                                             l.setFormSize(9f);
+                                             l.setTextSize(11f);
+                                             l.setXEntrySpace(4f);
+                                             // l.setExtra(ColorTemplate.VORDIPLOM_COLORS, new String[] { "abc",
+                                             // "def", "ghj", "ikl", "mno" });
+                                             // l.setCustom(ColorTemplate.VORDIPLOM_COLORS, new String[] { "abc",
+                                             // "def", "ghj", "ikl", "mno" });
+
+                                             setData(poll, mChart);
+                                         }
+
+                                         private void setData(Poll poll, BarChart mChart) {
+                                             int count = poll.keyCount.size();
+                                             Collection<Integer> values = poll.keyCount.values();
+                                             int max = 0;
+                                             for (Integer integer : values) {
+                                                 if (integer > max) {
+                                                     max = integer;
+                                                 }
+                                             }
+                                             float range = max;
+                                             Iterator<String> iterators = poll.keyCount.keySet().iterator();
+                                             ArrayList<String> xVals = new ArrayList<String>();
+                                             for (int i = 0; i < count; i++) {
+                                                 xVals.add(iterators.next());
+                                             }
+
+                                             ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
+
+                                             Iterator<Integer> iterator = poll.keyCount.values().iterator();
+                                             for (int i = 0; i < count; i++) {
+                                                 float val = (float) (iterator.next());
+                                                 yVals1.add(new BarEntry(val, i));
+                                             }
+
+                                             BarDataSet set1 = new BarDataSet(yVals1, poll.name);
+                                             set1.setBarSpacePercent(40f);
+
+                                             ArrayList<BarDataSet> dataSets = new ArrayList<BarDataSet>();
+                                             dataSets.add(set1);
+
+                                             BarData data = new BarData(xVals, dataSets);
+                                             // data.setValueFormatter(new MyValueFormatter());
+                                             data.setValueTextSize(10f);
+                                             data.setValueTypeface(mTf);
+
+                                             mChart.setData(data);
+                                         }
+                                     }
+
+        );
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
