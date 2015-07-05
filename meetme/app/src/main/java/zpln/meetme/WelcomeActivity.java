c5752b479250f937b74a340e695d51ce4b111fc2
Shoham Peller <shohamp@gmail.com>
2015-07-06 01:30:49 +0300

    Added handling a fb connect, and getting the connected user's name

--- meetme/app/src/main/java/zpln/meetme/WelcomeActivity.java
+++ meetme/app/src/main/java/zpln/meetme/WelcomeActivity.java
@@ -1,24 +1,46 @@
 package zpln.meetme;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class WelcomeActivity extends ActionBarActivity {
+    CallbackManager callbackManager;
+    final String log_tag = "LoginScreen";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_welcome);
+        callbackManager = CallbackManager.Factory.create();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -69,5 +91,47 @@ public class WelcomeActivity extends ActionBarActivity {
         TextView logo_txt = (TextView) findViewById(R.id.logo_txtview);
         Typeface amplify = Typeface.createFromAsset(getAssets(), "amplify.ttf");
         logo_txt.setTypeface(amplify);
+
+        authButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(log_tag, "Login Success");
+                /* make the API call */
+                new GraphRequest(
+                        AccessToken.getCurrentAccessToken(),
+                        "/"+loginResult.getAccessToken().getUserId(),
+                        null,
+                        HttpMethod.GET,
+                        new GraphRequest.Callback() {
+                            public void onCompleted(GraphResponse response) {
+                                JSONObject json_response = response.getJSONObject();
+                                if (json_response == null) {
+                                    Log.e(log_tag, "Error getting the graph response");
+                                }
+                                try {
+                                    Log.i(log_tag, String.format("User logged in. id \"%s\" name \"%s %s\"",
+                                            json_response.getString("id"),
+                                            json_response.getString("first_name"),
+                                            json_response.getString("last_name")));
+                                } catch (JSONException e) {
+                                    Log.e(log_tag, "Error getting the graph response");
+                                }
+                            }
+                        }
+                ).executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(log_tag, "Login Cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                Log.e(log_tag, String.format("Login Error:\n%s", e));
+                e.toString();
+            }
+        });
     }
 }
